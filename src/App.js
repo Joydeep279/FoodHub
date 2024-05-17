eedb44f38526a331cf6c65728fcbdca042a85291
Joydeep Nath <joydeepnath279@gmail.com>
2024-05-17 19:03:40 +0530

    navbar ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import Heading from "./components/Head";
 
 const AppLayout = () => {
@@ -10,6 +10,7 @@ const AppLayout = () => {
   );
 };
 
-const root = ReactDom.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(<AppLayout/>);
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<AppLayout />);
+  
--- src/components/Head.js
+++ src/components/Head.js
@@ -1,32 +1,24 @@
 import React from "react";
-
+const navbarTransition = "hover:bg-white rounded transition duration-200";
 const Heading = () => {
   return (
-    <div className="flex justify-between items-center px-10 py-5">
+    <div className="flex justify-between items-center px-11 py-6">
       <img
         className="w-28 h-28"
         src="https://static.vecteezy.com/system/resources/previews/017/133/166/original/eps10-grey-chicken-burger-hamburger-abstract-solid-art-icon-or-logo-isolated-on-black-background-fast-food-symbol-in-a-simple-flat-trendy-modern-style-for-your-website-design-and-mobile-app-vector.jpg"
         alt="logo"
       />
       <ul className="flex flex-row gap-10 text-[#666666] font-sans font-semibold text-4xl bg-[#F7F7F7] items-center px-2.5 py-1 rounded-md">
-        <li className="hover:bg-white rounded transition duration-300 ">
-          Home
-        </li>
-        <li className="hover:bg-white rounded transition duration-200 ">
-          Contact
-        </li>
-        <li className="hover:bg-white rounded transition duration-200 ">
-          About
-        </li>
-        <li className="hover:bg-white rounded transition duration-200 ">
-          Cart
-        </li>
+        <li className={navbarTransition}>Home</li>
+        <li className={navbarTransition}>Contact</li>
+        <li className={navbarTransition}>About</li>
+        <li className={navbarTransition}>Cart</li>
       </ul>
       <div className="flex justify-center items-center gap-5">
         <img
-          className="w-10 h-10 rounded-lg"
-          src="https://img.icons8.com/ios-filled/100/737373/name.png"
-          alt="name"
+          className="w-10 h-10"
+          src="https://img.icons8.com/ios-filled/100/737373/user-male-circle.png"
+          alt="user-male-circle"
         />
         <button className="bg-[#8E8E93] px-5 py-0.5 rounded text-white">
           LogIn
