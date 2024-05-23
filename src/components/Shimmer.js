7c52a3e4ba07d68d8cb1f5fabcde3ce49c173c4c
Joydeep Nath <joydeepnath279@gmail.com>
2024-05-23 10:36:45 +0530

    Improved shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -6,8 +6,8 @@ const Shimmer = () => {
       className="flex w-52 h-64 flex-col m-5 rounded-xl shadow overflow-hidden gap-5">
       <div className="w-52 h-36 rounded-lg bg-[#dee2e6]"></div>
       <div className="flex flex-col gap-5 px-2">
-        <div className="w-48 h-5 bg-[#e9ecef] rounded-md"></div>
-        <div className="w-44 h-5 bg-[#e9ecef] rounded-md"></div>
+        <div className="w-28 h-5 bg-[#e9ecef] rounded-md"></div>
+        <div className="w-24 h-5 bg-[#e9ecef] rounded-md"></div>
       </div>
     </div>
   ));
