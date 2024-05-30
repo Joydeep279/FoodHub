b9319ebc6e9828ae2947d70c5227dc20f25cfba5
Joydeep Nath <joydeepnath279@gmail.com>
2024-05-30 11:34:28 +0530

    added styling to menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,13 +7,18 @@ MenuList;
 const Menu = () => {
   const { id } = useParams();
   const menu = useMenu(id);
-  if (menu.length===0) {
+  if (menu.length === 0) {
     return <Shimmer />;
-  } 
-  else {
-    return menu.map((element) => (
-      <MenuList props={element} key={element.card.info.id} />
-    ));
+  } else {
+    return (
+      <div className="flex justify-center items-center flex-col gap-5">
+        <h1 className="font-sans font-extrabold text-gray-800 text-2xl">MENU</h1>
+
+        {menu.map((res) => {
+          return <MenuList props={res} key={res.card.info.id} />;
+        })}
+      </div>
+    );
   }
 };
 
--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,15 +1,21 @@
 const MenuList = (props) => {
-  const {imageId, name, price } = props.props.card.info;
+  console.log( props.props.card.info);
+  const { imageId, name, price, description } = props.props.card.info;
+
   return (
-    <div className="flex row">
+    <div className="flex w-1/2 flex-row  gap-10 pr-5 rounded-md items-center justify-between bg-[#F7F7F7]">
       <img
+        className="w-48 h-28 rounded-lg"
         src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" +
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
           imageId
         }
       />
-      <h1>{name}</h1>
-      <h2>Rs{price}</h2>
+      <div>
+        <h1 className="font-bold font-sans font text-2xl text-gray-700">{name}</h1>
+        <h1>{description}</h1>
+      </div>
+      <h2>Rs{price / 100}</h2>
     </div>
   );
 };
