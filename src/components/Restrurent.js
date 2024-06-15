793f766366bd751f66927754ae7077bca2a66c76
Joydeep Nath <joydeepnath279@gmail.com>
2024-06-15 12:09:07 +0530

    solved change in location after every api call

--- src/components/Restrurent.js
+++ src/components/Restrurent.js
@@ -4,10 +4,12 @@ import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import { RestData } from "../utils/useContext";
 import useRest from "../utils/useRest";
+let count = 0;
 const Restrurent = () => {
   const { Rest } = useContext(RestData);
-
-  useRest();
+  if (count === 0) {
+    useRest();
+  }
 
   if (Rest?.length === 0) {
     return <Shimmer />;
@@ -25,4 +27,3 @@ const Restrurent = () => {
 
 export default Restrurent;
 // add cart=> dispature action => reducer function => update the slice of the store.
-
